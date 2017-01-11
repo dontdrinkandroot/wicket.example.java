be263be813e3921091e2f27cc065accdc7c7ff6d
Philip Washington Sorst <philip@sorst.net>
2017-01-11 14:11:27 +0100

    Reflecting lib changes

--- src/main/java/net/dontdrinkandroot/example/ddrstack/page/DecoratorPage.java
+++ src/main/java/net/dontdrinkandroot/example/ddrstack/page/DecoratorPage.java
@@ -18,7 +18,7 @@ import net.dontdrinkandroot.extensions.wicket.bootstrap.headeritem.Dontdrinkandr
 import net.dontdrinkandroot.extensions.wicket.bootstrap.headeritem.DontdrinkandrootBootstrapCssHeaderItem;
 import net.dontdrinkandroot.wicket.behavior.CssClassAppender;
 import net.dontdrinkandroot.wicket.bootstrap.component.item.BookmarkablePageLinkItem;
-import net.dontdrinkandroot.wicket.bootstrap.component.item.DropDownItem;
+import net.dontdrinkandroot.wicket.bootstrap.component.item.SimpleDropDownItem;
 import net.dontdrinkandroot.wicket.bootstrap.css.BootstrapCssClass;
 import net.dontdrinkandroot.wicket.bootstrap.page.StandardBootstrapPage;
 import org.apache.wicket.Component;
@@ -30,131 +30,130 @@ import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
-
 public abstract class DecoratorPage<T> extends StandardBootstrapPage<T>
 {
+    public DecoratorPage(PageParameters parameters)
+    {
+        super(parameters);
+    }
 
-	public DecoratorPage(PageParameters parameters)
-	{
-		super(parameters);
-	}
+    public DecoratorPage(IModel<T> model)
+    {
+        super(model);
+    }
 
-	public DecoratorPage(IModel<T> model)
-	{
-		super(model);
-	}
+    @Override
+    protected Component createNavBar(String id)
+    {
+        Component navBar = super.createNavBar(id);
+        navBar.add(new CssClassAppender(BootstrapCssClass.NAVBAR_FIXED_TOP));
+        return navBar;
+    }
 
-	@Override
-	protected Component createNavBar(String id)
-	{
-		Component navBar = super.createNavBar(id);
-		navBar.add(new CssClassAppender(BootstrapCssClass.NAVBAR_FIXED_TOP));
-		return navBar;
-	}
+    @Override
+    protected void populateNavbarLeftItems(RepeatingView navItemView)
+    {
+        super.populateNavbarLeftItems(navItemView);
+        navItemView.add(new SimpleDropDownItem(navItemView.newChildId(), "Bootstrap")
+        {
+            @Override
+            protected boolean isActive()
+            {
+                return AbstractBootstrapPage.class.isAssignableFrom(this.getPage().getClass());
+            }
 
-	@Override
-	protected void populateNavbarLeftItems(RepeatingView navItemView)
-	{
-		super.populateNavbarLeftItems(navItemView);
-		navItemView.add(new DropDownItem(navItemView.newChildId(), "Bootstrap") {
-
-			@Override
-			protected boolean isActive()
-			{
-				return AbstractBootstrapPage.class.isAssignableFrom(this.getPage().getClass());
-			}
-
-			@Override
-			protected void populateItems(RepeatingView itemView)
-			{
-				itemView.add(
-						new BookmarkablePageLinkItem(
-								itemView.newChildId(),
+            @Override
+            protected void populateItems(RepeatingView itemView)
+            {
+                itemView.add(
+                        new BookmarkablePageLinkItem(
+                                itemView.newChildId(),
                                 Model.of("Font Awesome Icons"),
-                                FontAwesomeIconPage.class));
+                                FontAwesomeIconPage.class
+                        ));
                 itemView.add(new BookmarkablePageLinkItem(itemView.newChildId(), Model.of("Icons"), IconPage.class));
-
-			}
-		});
-		navItemView.add(
-				new BookmarkablePageLinkItem(navItemView.newChildId(), Model.of("Calendar"), CalendarPage.class));
+            }
+        });
+        navItemView.add(
+                new BookmarkablePageLinkItem(navItemView.newChildId(), Model.of("Calendar"), CalendarPage.class));
         navItemView.add(new BookmarkablePageLinkItem(
                 navItemView.newChildId(),
                 Model.of("Resources"),
                 ResourcesPage.class
         ));
         navItemView.add(
-				new BookmarkablePageLinkItem(
-						navItemView.newChildId(),
+                new BookmarkablePageLinkItem(
+                        navItemView.newChildId(),
                         Model.of("Forms"),
-						FormPage.class
-				));
-		navItemView.add(new BookmarkablePageLinkItem(
+                        FormPage.class
+                ));
+        navItemView.add(new BookmarkablePageLinkItem(
                 navItemView.newChildId(),
                 Model.of("JavaScript"),
                 CallbackPage.class
         )
         {
-
-			@Override
-			protected boolean isActive()
-			{
-				return AbstractJavascriptPage.class.isAssignableFrom(this.getPage().getClass());
-			}
-		});
-		navItemView.add(new DropDownItem(navItemView.newChildId(), "Components") {
-
-			@Override
-			protected void populateItems(RepeatingView itemView)
-			{
-				itemView.add(
-						new BookmarkablePageLinkItem(
-								itemView.newChildId(),
+            @Override
+            protected boolean isActive()
+            {
+                return AbstractJavascriptPage.class.isAssignableFrom(this.getPage().getClass());
+            }
+        });
+        navItemView.add(new SimpleDropDownItem(navItemView.newChildId(), "Components")
+        {
+            @Override
+            protected void populateItems(RepeatingView itemView)
+            {
+                itemView.add(
+                        new BookmarkablePageLinkItem(
+                                itemView.newChildId(),
                                 Model.of("PageHeight Scaling"),
-                                PageHeightScalingPage.class));
+                                PageHeightScalingPage.class
+                        ));
                 itemView.add(new BookmarkablePageLinkItem(
                         itemView.newChildId(),
                         Model.of("Lifecycle"),
                         LifecyclePage.class
                 ));
                 itemView.add(
-						new BookmarkablePageLinkItem(
-								itemView.newChildId(),
-								Model.of("Border and Fragment"),
-								FragmentBorderPage.class));
-			}
-		});
+                        new BookmarkablePageLinkItem(
+                                itemView.newChildId(),
+                                Model.of("Border and Fragment"),
+                                FragmentBorderPage.class
+                        ));
+            }
+        });
 
         navItemView.add(new BookmarkablePageLinkItem(navItemView.newChildId(), Model.of("Cookies"), CookiePage.class));
         navItemView.add(new BookmarkablePageLinkItem(navItemView.newChildId(), Model.of("Events"), EventPage.class));
     }
 
-	@Override
-	protected Component createBrand(String id)
-	{
-		BookmarkablePageLink<Void> brand = new BookmarkablePageLink<Void>(id, WicketApplication.get().getHomePage());
-		brand.setBody(Model.of("dontdrinkandroot"));
+    @Override
+    protected Component createBrand(String id)
+    {
+        BookmarkablePageLink<Void> brand = new BookmarkablePageLink<Void>(id, WicketApplication.get().getHomePage());
+        brand.setBody(Model.of("dontdrinkandroot"));
 
-		return brand;
-	}
+        return brand;
+    }
 
-	@Override
-	public void renderHead(IHeaderResponse response)
-	{
-		response.render(new DontdrinkandrootBootstrapCssHeaderItem());
-		response.render(new DontdrinkandrootBootstrap33JsHeaderItem(true));
-		response.render(CssHeaderItem.forCSS("body {padding-top: 60px}", "bodyPadding"));
-	}
+    @Override
+    public void renderHead(IHeaderResponse response)
+    {
+        response.render(new DontdrinkandrootBootstrapCssHeaderItem());
+        response.render(new DontdrinkandrootBootstrap33JsHeaderItem(true));
+        response.render(CssHeaderItem.forCSS("body {padding-top: 60px}", "bodyPadding"));
+    }
 
-	@Override
-	protected IModel<String> createPageTitlePrefixModel()
-	{
-		return Model.of("dontdrinkandroot");
-	}
+    @Override
+    protected IModel<String> createPageTitlePrefixModel()
+    {
+        return Model.of("dontdrinkandroot");
+    }
 
-	@Override
-	protected IModel<String> createPageHeadingModel()
-	{
-		return new Model<String>(this.getClass().getSimpleName());
-	}
+    @Override
+    protected IModel<String> createPageHeadingModel()
+    {
+        return new Model<String>(this.getClass().getSimpleName());
+    }
 }
--- src/main/java/net/dontdrinkandroot/example/ddrstack/page/component/PageHeightScalingPage.java
+++ src/main/java/net/dontdrinkandroot/example/ddrstack/page/component/PageHeightScalingPage.java
@@ -1,23 +1,19 @@
 package net.dontdrinkandroot.example.ddrstack.page.component;
 
 import net.dontdrinkandroot.example.ddrstack.page.DecoratorPage;
-import net.dontdrinkandroot.wicket.behavior.PageHeightScalingBehavior;
+import net.dontdrinkandroot.wicket.extras.behavior.PageHeightScalingBehavior;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
-
 public class PageHeightScalingPage extends DecoratorPage<Void>
 {
+    public PageHeightScalingPage(PageParameters parameters)
+    {
+        super(parameters);
 
-	public PageHeightScalingPage(PageParameters parameters)
-	{
-
-		super(parameters);
-
-		WebMarkupContainer scaling = new WebMarkupContainer("scaling");
-		scaling.add(new PageHeightScalingBehavior());
-		scaling.setOutputMarkupId(true);
-		this.add(scaling);
-	}
-
+        WebMarkupContainer scaling = new WebMarkupContainer("scaling");
+        scaling.add(new PageHeightScalingBehavior());
+        scaling.setOutputMarkupId(true);
+        this.add(scaling);
+    }
 }
